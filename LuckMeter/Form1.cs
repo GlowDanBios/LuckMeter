a53cd2e9f69a6161272662081c2220a0ad44174a
GlowDanBios <46843591+GlowDanBios@users.noreply.github.com>
2020-05-12 15:00:08 +0300

    code

--- LuckMeter/Form1.cs
+++ LuckMeter/Form1.cs
@@ -10,11 +10,44 @@ using System.Windows.Forms;
 
 namespace LuckMeter
 {
-  public partial class Form1 : Form
-  {
-    public Form1()
+    public partial class Form1 : Form
     {
-      InitializeComponent();
+        private int num;
+        private int times;
+        public Form1()
+        {
+            InitializeComponent();
+            num = new Random().Next(1, 11);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int input;
+            bool success = int.TryParse(this.textBox1.Text, out input);
+            if (success)
+            {
+                if (input == num || times == 10)
+                {
+                    this.label3.Text = "Коэффицент везения: " + ((double)num / 10).ToString();
+                    this.label2.Text = "Вы угдали";
+                }
+                else
+                {
+                    
+                    this.label2.Text = "Не угадали";
+                    this.label2.Left += 50;
+                    times++;
+                }
+            }
+        }
     }
-  }
 }
